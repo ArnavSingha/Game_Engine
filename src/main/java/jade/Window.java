05e54f64ac9c5f33fcb40de327c9b4457d6ea097
ArnavSingha <102987879+ArnavSingha@users.noreply.github.com>
2025-04-08 02:24:05 +0530

    day2 window

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -3,12 +3,15 @@ package jade;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWErrorCallback;
-
+import org.lwjgl.opengl.GL;
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
     private int width, height;
     private String title;
+    private long glfwWindow;
 
     private static Window window = null;
 
@@ -46,9 +49,30 @@ public class Window {
         glfwWindowHint(GLFW_RESIZABLE,GLFW_TRUE);
         glfwWindowHint(GLFW_MAXIMIZED,GLFW_TRUE);
 
+//        Create the window
+
+        glfwWindow = glfwCreateWindow(this.width,this.height,this.title,NULL,NULL);
+        if (glfwWindow == NULL){
+            throw new IllegalStateException("Failed to create the glfw window");
+        }
+
+//        Make OpenGl the context current
+        glfwMakeContextCurrent(glfwWindow);
+//        Enable v-sync
+        glfwSwapInterval(1);
+
+//        Make the window visible
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities();
     }
 
-    public void loop(){
-
+    public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            glfwPollEvents();
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(glfwWindow);
+        }
     }
 }
